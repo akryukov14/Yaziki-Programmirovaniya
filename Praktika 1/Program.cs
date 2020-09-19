b518dcb153dd131b30dd432e3f11a153db692bee
Andrey Kryukov <kryukov1189@mail.ru>
2020-09-19 21:52:39 +0300

    Added Gender

--- Praktika 1/Program.cs
+++ Praktika 1/Program.cs
@@ -8,18 +8,22 @@ namespace Praktika_1
 {
     struct Person
     {
+        public enum Genders : int { Male, Female };
         public string firstName;
         public string lastName;
         public int age;
+        public Genders gender;
 
-        public Person(string _firstName, string _lastName, int _age)
+        public Person(string _firstName, string _lastName, int _age, Genders _gender)
         {
             firstName = _firstName;
-            lastName = _lastName; age = _age;
+            lastName = _lastName; 
+            age = _age;
+            gender = _gender;
         }
         public override string ToString()
         {
-            return firstName + " " + lastName + ", age " + age;
+            return firstName + " " + lastName + " (" + gender + "), age " + age;
         }
 
     }
@@ -29,7 +33,7 @@ namespace Praktika_1
 
         static void Main(string[] args)
         {
-            Person p = new Person("Andrey", "Kryukov", 22); 
+            Person p = new Person("Andrey", "Kryukov", 22, Person.Genders.Male); 
             Console.WriteLine(p);
             Console.ReadKey();
         }
