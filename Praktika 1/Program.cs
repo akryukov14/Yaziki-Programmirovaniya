982a7b487895e02f596b56e4090541888988daed
Andrey Kryukov <kryukov1189@mail.ru>
2020-09-19 21:28:45 +0300

    Добавьте файлы проекта.

---
+++ Praktika 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_1
+{
+    struct Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public Person(string _firstName, string _lastName, int _age)
+        {
+            firstName = _firstName;
+            lastName = _lastName; age = _age;
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName + ", age " + age;
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person p = new Person("Andrey", "Kryukov", 22); 
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
