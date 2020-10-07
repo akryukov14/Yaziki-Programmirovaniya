49375a43df6234b468730f7cc327ac00e3fe5c02
akryukov14 <kryukov1189@mail.ru>
2020-10-07 11:37:16 +0300

    added praktika 04-1/04-2

---
+++ Praktika 4/praktika 4-01/praktika 4-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace prakt4_01
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList() { "Jora" }                ;
+      myList.Add("Andrey");
+      myList.Add("Mikhalich");
+      myList.Add("Whiskey");
+      myList.Add("C#");
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      // Сортировка при помощи стандартного объекта сравнения 
+      myList.Sort();
+      //myList.Reverse();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+
+    }
+
+  }
+}
---
+++ Praktika 4/praktika 4-02/praktika 4-02/praktika 4-02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace prak4_02_upr02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Stack stack = new Stack();
+      stack.Push("Andrey1");
+      stack.Push(0.5);
+      stack.Push(2);
+      stack.Push("Andrey4");
+      stack.Push("Andrey5");
+      stack.Push("Jora");
+      stack.Push("Sanya");
+      while (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("'From Stack: {0}", obj);
+        //Console.WriteLine("SEDIT: {0}", stack.Peek());
+      }
+
+    }
+  }
+}
